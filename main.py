30e3d78b9de3d8523960fb856edb4f4fad9c76ca
JesapIt <119874282+JesapIt@users.noreply.github.com>
2023-02-26 21:34:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,59 +28,62 @@ client = gspread.authorize(creds)
 
 st.markdown('## Area Business Development')
 st.markdown('### [Link google sheet](https://docs.google.com/spreadsheets/d/1ioR9yXMCJJxuxaiuEBoPKP4-EJLa6RgdYZicrTQpxl0/edit#gid=0)')
+link_BD = "https://docs.google.com/spreadsheets/d/1ioR9yXMCJJxuxaiuEBoPKP4-EJLa6RgdYZicrTQpxl0/edit#gid=0"
+sht = client.open_by_url(link_BD)
 
+# funzione al click del bottone
+def fun():
+	st.session_state.multi = []
+	if nome != '':
+		double = 0
+		for w in sht.worksheets():
+			lower_title = w.title.lower()
+			lower_name = nome.lower()
+			if lower_name in lower_title:
+				double +=1
+				current_work = w
+		if double == 0:
+			st.error('Nessuna risorsa trovata con questo nome/cognome')
+
+		elif double > 1:
+			st.warning('Sono state trovate più risorse con questo nome/cognome, cerca di essre più specifico')
+		else:
+		# --- adding elements to google sheet ---
+			def next_available_row(worksheet):
+				str_list = list(filter(None, worksheet.col_values(1)))
+				return str(len(str_list)+1)
+
+			for a in temp_att:
+				row = next_available_row(current_work)
+				c1 = Cell(int(row) , 1, str(data))
+				c2 = Cell(int(row) , 2, a)
+				c3 = Cell(int(row) , 3, str(dictionary[a]).replace(':', '.'))
+				current_work.update_cells([c1,c2,c3], value_input_option='USER_ENTERED')
+				#current_work.update_cell(row , 1, str(data))
+				#current_work.update_cell(row , 2, a)
+				#current_work.update_cell(row , 3, str(dictionary[a]).replace(':', '.'))
+
+			st.success(f'Conteggio ore di {current_work.title} aggiornato')
+	return
 
 # --- Interfaccia ----
 nome = st.text_input('Nome e/o Cognome')
 data = st.date_input('Data', value=date.today())
-data = data.strftime("%d/%m/%Y")
-options = ['call', 'formazione', 'task', 'progetto', 'altro']
-att = st.multiselect('Attività', options)
+options = ['Call d\'area', 'Assemblea mensile', 'Delega', 'Recruiting', 'Mentoring', 'Progetto esterno'
+,'Progetto interno', 'Formazione', 'Call HR buddy', 'Organizzazione area', 'Task interno', 'Evento', 'Business proposal', 'Preventivo', 'Prima call lead', 'Call follow-up lead', 'Lead generation', 'Risposta mail cliente']
+
+att = st.multiselect('Attività', options, key="multi")
 dictionary = {}
 for a in att:
 	n_ore = st.time_input(f'Numero di ore {a}', datetime.time(1, 0), key=a)
 	dictionary[a] = n_ore
 
-sub = st.button("Invia")
+data = data.strftime("%d/%m/%Y")
+temp_att = att
+sub = st.button("Invia",on_click=fun)
 
 
 
-
-
-link_BD = "https://docs.google.com/spreadsheets/d/1ioR9yXMCJJxuxaiuEBoPKP4-EJLa6RgdYZicrTQpxl0/edit#gid=0"
-sht = client.open_by_url(link_BD)
-# -- Selecting current worksheet ---
-if sub and nome != '':
-	double = 0
-	for w in sht.worksheets():
-		lower_title = w.title.lower()
-		lower_name = nome.lower()
-		if lower_name in lower_title:
-			double +=1
-			current_work = w
-	if double == 0:
-		st.error('Nessuna risorsa trovata con questo nome/cognome')
-
-	elif double > 1:
-		st.warning('Sono state trovate più risorse con questo nome/cognome, cerca di essre più specifico')
-	else:
-	# --- adding elements to google sheet ---
-		def next_available_row(worksheet):
-			str_list = list(filter(None, worksheet.col_values(1)))
-			return str(len(str_list)+1)
-
-		for a in att:
-			row = next_available_row(current_work)
-			c1 = Cell(int(row) , 1, str(data))
-			c2 = Cell(int(row) , 2, a)
-			c3 = Cell(int(row) , 3, str(dictionary[a]).replace(':', '.'))
-			current_work.update_cells([c1,c2,c3], value_input_option='USER_ENTERED')
-			#current_work.update_cell(row , 1, str(data))
-			#current_work.update_cell(row , 2, a)
-			#current_work.update_cell(row , 3, str(dictionary[a]).replace(':', '.'))
-
-
-		st.success(f'Conteggio ore di {current_work.title} aggiornato')
 	
 
 
